2631fb6821c2a8c02c274eb37b9b9ca584c663a8
sajak <sajakbasnet77@gmail.com>
2024-03-01 14:54:45 +0545

    changed summerizing method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import  express  from "express";
 import dotenv from "dotenv";
 import axios from "axios";
+import { getSubtitles } from 'youtube-captions-scraper';
 
 dotenv.config();
 
@@ -20,37 +21,76 @@ app.get('/' , (req , res) => {
     res.render("app");
 })
 
-app.post('/summerize' , async (req , res) => {
-    let viedoUrl = req.body.viedoUrl;
+const getVideoId = (videoUrl)=> {
+  const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
 
-    if(viedoUrl === undefined || viedoUrl === '' || viedoUrl === null ){
-        return res.render('app', {validFlag : false , message : "please enter valid video URL"})
-    } else {
-        //hit an api endpoint
+const match = videoUrl.match(regex);
+
+const result = match && match[1];
+
+return result;
+
+}
+
+
+const extractText = async (viedoId) => {
+  try {
+  const captions = await getSubtitles({
+      videoID: `${viedoId}`, // youtube video id
+          lang: 'en' // default: `en`
+        })
+        const concatenatedText = captions.reduce((acc , current) => {
+          return acc + current.text;
+        })
+        return concatenatedText;
+  } catch (error) {
+      console.log(error.meessage);
+  }
+}
+
+
+//extract videoId form URL ---> using regex patterns
+app.post('/summerize' , async (req , res) => {
+  let url = req.body.viedoUrl;
+  // if(!url)  return res.status(400).json({ error: "Video URL is missing in the request body." });
+  if(url === undefined || url === '' || url === null ){
+    return res.render('app', {validFlag : false , message : "please enter valid video URL"})
+  } else {
+    //hit an api endpoint
+    
+        let viedoId = getVideoId(url);
+        // console.log("ID: ",viedoId);
+        const text = await extractText(viedoId);
+        // console.log("text: ",text);
+
         const options = {
-            method: 'GET',
-            url: 'https://youtube-summarizer-by-chatgpt.p.rapidapi.com/ytsummary1/',
-            params: {
-              url: `${viedoUrl}`,
-              key: process.env.GPT_KEY
-            },
+            method: 'POST',
+            url: 'https://gpt-summarization.p.rapidapi.com/summarize',
             headers: {
-              'X-RapidAPI-Key': process.env.API_KEY,
-              'X-RapidAPI-Host': process.env.API_HOST
+              'content-type': 'application/json',
+              'X-RapidAPI-Key': '71d2e5926bmsh6f68f198a07cda5p162612jsne71114d195e3',
+              'X-RapidAPI-Host': 'gpt-summarization.p.rapidapi.com'
+            },
+            data: {
+              text: `${text}`,
+              num_sentences: 30
             }
           };
           
           try {
-              const response = await axios.request(options);
-            //   console.log(response.data.summary);
-            if (response) {
-                res.render("app", {validFlag : true , summerizeText : response.data.summary});
+            const response = await axios.request(options);
+            // console.log("response",response);
+            if(response) {
+              res.render("app", {validFlag : true , summerizeText : response.data.summary});
             }
           } catch (error) {
-                res.render("app" , {validFlag : false ,summerizeText : "Provided viedo does not have transcripts" })
+            res.render("app" , {validFlag : false ,summerizeText : "Provided viedo does not have transcripts" })
           }
+          };
+          
+
     }
-})
+)
 
 app.listen(PORT , () => {
     console.log("Server running");
